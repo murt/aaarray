ba62e90f3e1062d7c8f478453df4ce17c71a0172
murt <murt@unpossible.io>
2019-06-09 22:13:25 +1000

    WIP of getting the dynamic type returns to actually work.

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,95 @@
-// TODO: Index accessors? Supporting for-in or for-of? Synchronous functions
-export interface AAArray<T> {
-    readonly length: number,
-    map(callback: AAMapDelegate<T>): Promise<AAArray<T>>;
-    toArray(): T[];
-    toString(): string;
-    [index: number]: T;
-}
-
 export interface AAMapDelegate<T> {
     (value: T, index: number, arr: T[]): T | Promise<T>;
 }
 
+export interface AAFilterDelegate<T> {
+    (value: T, index: number, arr: T[]): Boolean | T | Promise<Boolean> | Promise<T>;
+}
+
+type AADelegate<T> = AAMapDelegate<T> | AAFilterDelegate<T>;
+
+const enum AAction {
+    MAP = "map",
+    FILTER = "filter",
+};
+
+interface AAActionEntry<T> {
+    action: AAction;
+    delegate: AADelegate<T>;
+}
+
+export class AAArray<T> {
+    private array: T[];
+    private readonly queue: AAActionEntry<T>[];
+
+    public constructor(array: Array<T>) {
+        this.array = array;
+        this.queue = [];
+    }
+
+    public get length() {
+        return this.array.length;
+    }
+
+    public toArray(): T[] {
+        return this.array;
+    }
+
+    public toString(): string {
+        return this.array.toString();
+    }
+
+    public map(callback: AAMapDelegate<T>): AAArray<any> {
+        this.action(AAction.MAP, callback);
+        return this as AAArray<any>;
+    }
+
+    public filter(callback: AAFilterDelegate<T>): AAArray<T> {
+        this.action(AAction.FILTER, callback);
+        return this;
+    }
+
+    public async run(): Promise<AAArray<T>> {
+        while (this.queue.length) {
+            await this.runAction(this.queue.shift() as AAActionEntry<T>);
+        }
+        return this;
+    }
+
+    public async value(): Promise<T[]> {
+        return (await this.run()).toArray();
+    }
+
+    private action(action: AAction, delegate: AADelegate<T>): void {
+        this.queue.push({ action, delegate });
+    }
+
+    private async runAction(entry: AAActionEntry<T>): Promise<void> {
+        switch (entry.action) {
+            case AAction.MAP:
+                await this.runMap(entry.delegate as AAMapDelegate<T>);
+                break;
+            case AAction.FILTER:
+                await this.runFilter(entry.delegate as AAFilterDelegate<T>);
+                break
+        }
+    }
+
+    private async runMap(callback: AAMapDelegate<T>): Promise<void> {
+        this.array = await Promise.all(this.array.map(callback));
+    }
+
+    private async runFilter(callback: AAFilterDelegate<T>): Promise<void> {
+        this.array = await Promise.all(this.array.filter(callback));
+    }
+}
+
 /**
  * Entrypoint for all aaarray functionality.
- * 
+ *
  * @param array Array value that will be used to initialise the AAArray
  * @returns New AAArray object
  */
 export default function AA<U>(array: Array<U>): AAArray<U> {
-    return {
-        length: array.length,
-        map: async (callback: AAMapDelegate<U>) => {
-            return await Promise.all(array.map(callback)).then(AA);
-        },
-        toArray: () => array,
-        toString: () => array.toString(),
-    };
+    return new AAArray<U>(array);
 }
--- test/map.test.ts
+++ test/map.test.ts
@@ -1,19 +1,45 @@
-import AA from "../src";
+import AA, { AAArray } from "../src";
 
 describe("aaarray#map", () => {
     it("should support async functions", async () => {
-        const results = await AA([1, 2, 3]).map(
-            async n =>
-                await new Promise<typeof n>((resolve, reject) => {
-                    setTimeout(() => resolve(n * 2), 500);
-                })
-        );
+        const results = await AA([1, 2, 3])
+            .map(
+                async n =>
+                    await new Promise<number>((resolve, reject) => {
+                        setTimeout(() => resolve(n * 2), 500);
+                    })
+            )
+            .value();
 
-        expect(results.toArray()).toEqual([2, 4, 6]);
+        expect(results).toEqual([2, 4, 6]);
     });
 
     it("should support sync functions", async () => {
-        const results = await AA([1, 2, 3]).map(n => n * 2);
-        expect(results.toArray()).toEqual([2, 4, 6]);
+        const results = await AA([1, 2, 3])
+            .map(n => n * 2)
+            .value();
+        expect(results).toEqual([2, 4, 6]);
+    });
+
+    it("should support async functions without await", async () => {
+        const results = await AA([1, 2, 3])
+            .map(async n => n * 2)
+            .value();
+        expect(results).toEqual([2, 4, 6]);
+    });
+
+    it("should support chained maps", async () => {
+        const results = await AA([1, 2, 3])
+            .map(async n => n * 2)
+            .map(async n => n * 3)
+            .value();
+        expect(results).toEqual([6, 12, 18]);
+    });
+
+    it("should support multiple types", async () => {
+        const results = await AA([1, 2, 3])
+            .map(async n => `test-${n}`)
+            .value();
+        expect(results).toEqual(["test1", "test2", "test3"]);
     });
 });

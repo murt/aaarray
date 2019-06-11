f1e9b2ee280c93ae6665447467e51bb9b7ee06a9
murt <murt@unpossible.io>
2019-06-11 20:51:01 +1000

    Still working on getting the chaining to have the correct types.
    Seems really hard to get the generic type out and re-apply it to a new array entry.

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,39 @@
 export interface AAMapDelegate<T> {
-    (value: T, index: number, arr: T[]): T | Promise<T>;
+    (value: T, index: number, arr: T[]): unknown | Promise<unknown>;
 }
 
 export interface AAFilterDelegate<T> {
-    (value: T, index: number, arr: T[]): Boolean | T | Promise<Boolean> | Promise<T>;
+    (value: T, index: number, arr: T[]): any;
 }
 
 type AADelegate<T> = AAMapDelegate<T> | AAFilterDelegate<T>;
 
-const enum AAction {
-    MAP = "map",
-    FILTER = "filter",
-};
-
-interface AAActionEntry<T> {
-    action: AAction;
-    delegate: AADelegate<T>;
+enum AAAction {
+    MAP,
+    FILTER,
 }
 
 export class AAArray<T> {
-    private array: T[];
-    private readonly queue: AAActionEntry<T>[];
 
-    public constructor(array: Array<T>) {
+    protected action?: { name: AAAction, delegate: AADelegate<T> };
+
+    protected readonly parent?: AAArray<any>;
+
+    protected readonly array: Array<T>;
+
+    public constructor(array: Array<T>, parent?: AAArray<any>) {
+        this.parent = parent;
         this.array = array;
-        this.queue = [];
     }
 
-    public get length() {
-        return this.array.length;
-    }
-
-    public toArray(): T[] {
-        return this.array;
-    }
-
-    public toString(): string {
-        return this.array.toString();
-    }
-
-    public map(callback: AAMapDelegate<T>): AAArray<any> {
-        this.action(AAction.MAP, callback);
-        return this as AAArray<any>;
-    }
-
-    public filter(callback: AAFilterDelegate<T>): AAArray<T> {
-        this.action(AAction.FILTER, callback);
-        return this;
-    }
-
-    public async run(): Promise<AAArray<T>> {
-        while (this.queue.length) {
-            await this.runAction(this.queue.shift() as AAActionEntry<T>);
-        }
-        return this;
+    public map(callback: AAMapDelegate<T>): AAArray<unknown> {
+        this.action = { name: AAAction.MAP, delegate: callback };
+        type response = ReturnType<typeof callback>;
+        return new AAArray(this.array as response[], this);
     }
 
     public async value(): Promise<T[]> {
-        return (await this.run()).toArray();
+        return Promise.resolve(this.array); 
     }
 
-    private action(action: AAction, delegate: AADelegate<T>): void {
-        this.queue.push({ action, delegate });
-    }
-
-    private async runAction(entry: AAActionEntry<T>): Promise<void> {
-        switch (entry.action) {
-            case AAction.MAP:
-                await this.runMap(entry.delegate as AAMapDelegate<T>);
-                break;
-            case AAction.FILTER:
-                await this.runFilter(entry.delegate as AAFilterDelegate<T>);
-                break
-        }
-    }
-
-    private async runMap(callback: AAMapDelegate<T>): Promise<void> {
-        this.array = await Promise.all(this.array.map(callback));
-    }
-
-    private async runFilter(callback: AAFilterDelegate<T>): Promise<void> {
-        this.array = await Promise.all(this.array.filter(callback));
-    }
-}
-
-/**
- * Entrypoint for all aaarray functionality.
- *
- * @param array Array value that will be used to initialise the AAArray
- * @returns New AAArray object
- */
-export default function AA<U>(array: Array<U>): AAArray<U> {
-    return new AAArray<U>(array);
 }

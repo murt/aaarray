9a690a7f01f538005445ca3946b557b4640eadd7
murt <murt@unpossible.io>
2019-06-11 21:08:38 +1000

    Types are now working so now a better structure to async resolve the value can be put in place.
    Will not work currently to actually calculate the values.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export interface AAMapDelegate<T> {
-    (value: T, index: number, arr: T[]): unknown | Promise<unknown>;
+    (value: T, index: number, arr: T[]): T | Promise<T>;
 }
 
 export interface AAFilterDelegate<T> {
     (value: T, index: number, arr: T[]): any;
 }
 
-type AADelegate<T> = AAMapDelegate<T> | AAFilterDelegate<T>;
+type AADelegate<T> = AAMapDelegate<T> | AAFilterDelegate<T> | any;
 
 enum AAAction {
     MAP,
@@ -19,21 +19,24 @@ export class AAArray<T> {
 
     protected readonly parent?: AAArray<any>;
 
-    protected readonly array: Array<T>;
+    protected readonly array?: Array<T> | null;
 
-    public constructor(array: Array<T>, parent?: AAArray<any>) {
+    public constructor(array?: Array<T>, parent?: AAArray<any>) {
         this.parent = parent;
         this.array = array;
     }
 
-    public map(callback: AAMapDelegate<T>): AAArray<unknown> {
+    public map<U>(callback: (value: T, index: number, array: T[]) => U | Promise<U>): AAArray<U> {
         this.action = { name: AAAction.MAP, delegate: callback };
-        type response = ReturnType<typeof callback>;
-        return new AAArray(this.array as response[], this);
+        return new AAArray<U>(undefined, this);
     }
 
     public async value(): Promise<T[]> {
-        return Promise.resolve(this.array); 
+        return Promise.resolve([]); 
     }
 
+}
+
+export default function AA<U>(array: Array<U>): AAArray<U> {
+    return new AAArray<U>(array);
 }

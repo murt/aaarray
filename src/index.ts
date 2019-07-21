fb46fd2f2748c34ffa5a07e3c5e8de02be6fa343
murt <murt@unpossible.io>
2019-07-21 21:59:12 +1000

    Made callback params optional.
    Added strong types to the runner functions. Added support for generic transforms that take params and not functions (eg. slice).

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-export type AAMapCallback<T, U> = (value: T, index: number, array: T[]) => U | Promise<U>;
+export type AAMapCallback<T, U> = (value?: T, index?: number, array?: T[]) => U | Promise<U>;
 
-export type AAFilterCallback<T> = (value: T, index: number, arr: T[]) => any;
+export type AAFilterCallback<T> = (value?: T, index?: number, arr?: T[]) => any;
+
+export type AATransform = (arr: any[]) => any[];
 
 // TODO: Investigate if we can actually see the full type of the callback as this might be handy in autocomp
 // down the line - right now it only shows AAMapCallback for example instead of all the params actually needed.
-type AACallback<T, U> = AAMapCallback<T, U> | AAFilterCallback<T>;
+type AACallback<T, U> = AAMapCallback<T, U> | AAFilterCallback<T> | AATransform;
 
 enum AAAction {
     MAP,
     FILTER,
+    SLICE,
 }
 
-type AAActionDelegate = { action: AAAction; callback: AACallback<any, any>; ordered: boolean };
+type AAActionDelegate<C = AACallback<any, any>> = { action: AAAction; callback: C; ordered: boolean };
 
 export class AAArray<T> implements PromiseLike<T[]> {
     protected readonly array: T[];
@@ -43,6 +46,15 @@ export class AAArray<T> implements PromiseLike<T[]> {
         return this;
     }
 
+    public slice(begin?: number, end?: number): AAArray<T> {
+        this.queue.push({ action: AAAction.SLICE, callback: (arr: any[]) => arr.slice(begin, end), ordered: true });
+        return this;
+    }
+
+    // TODO: find, findOrdered, slice, splice, join, reduce, reduceOrdered, reduceRightOrdered, concat, includes,
+    // indexOf, lastIndexOf, findIndex, fill, flat, some, flatMap, pop, reverse, sort, unshift, forEach, push,
+    // shift, every
+
     public async value(): Promise<T[]> {
         let arr = this.array;
         while (this.queue.length) {
@@ -57,7 +69,7 @@ export class AAArray<T> implements PromiseLike<T[]> {
     ): Promise<TResult1 | TResult2> {
         try {
             const value = await this.value();
-            return onfulfilled ? onfulfilled(value) : value as any;
+            return onfulfilled ? onfulfilled(value) : (value as any);
         } catch (err) {
             if (onrejected) {
                 return onrejected(err);
@@ -74,15 +86,17 @@ export class AAArray<T> implements PromiseLike<T[]> {
     private async run(arr: any[], action: AAActionDelegate): Promise<any[]> {
         switch (action.action) {
             case AAAction.MAP:
-                return this.runMap(arr, action);
+                return this.runMap(arr, action as AAActionDelegate<AAMapCallback<T, any>>);
             case AAAction.FILTER:
-                return this.runFilter(arr, action);
+                return this.runFilter(arr, action as AAActionDelegate<AAFilterCallback<T>>);
+            case AAAction.SLICE:
+                return this.runTransform(arr, action);
             default:
                 return arr;
         }
     }
 
-    private async runMap(arr: any[], action: AAActionDelegate): Promise<any[]> {
+    private async runMap(arr: any[], action: AAActionDelegate<AAMapCallback<T, any>>): Promise<any[]> {
         if (action.ordered) {
             for (let i = 0; i < arr.length; ++i) {
                 arr[i] = await action.callback(arr[i], i, arr);
@@ -93,7 +107,7 @@ export class AAArray<T> implements PromiseLike<T[]> {
         }
     }
 
-    private async runFilter(arr: any[], action: AAActionDelegate): Promise<any[]> {
+    private async runFilter(arr: any[], action: AAActionDelegate<AAFilterCallback<T>>): Promise<any[]> {
         let result = new Array(arr.length);
         if (action.ordered) {
             for (let i = 0; i < arr.length; ++i) {
@@ -103,6 +117,10 @@ export class AAArray<T> implements PromiseLike<T[]> {
             result = await Promise.all(arr.map(action.callback));
         }
         return result.reduceRight((prev, cur, i) => (cur ? prev : [...prev.slice(0, i), ...prev.slice(i + 1)]), arr);
+    }
+
+    private async runTransform(arr: any[], action: AAActionDelegate<AATransform>): Promise<any[]> {
+        return action.callback(arr);
     }
 }
 

644b5fb6572378299a2e19533f8ce17169fecfa3
murt <murt@unpossible.io>
2019-12-19 15:17:39 +1000

    Fixed some cross-browser differences with regards to local files by creating a local server for testing. In particular IE would block content for local files that required a GUI action to take place to allow it. Added Edge driver and test goal, don't know if it needs a driver from Nuget. Bumped the IEDriver back up to the latest

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,15 +1,42 @@
 import path from "path";
 
 import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
+import { Server } from "node-static";
+import http from "http";
 
 const BROWSER = process.env.BROWSER || "chrome";
+
+declare var window: any;
 
 describe("aaarray#xbrowser", () => {
     // Set the timeout for all tests to be longer for the sake of CI systems
     jest.setTimeout(30000);
 
+    // Re-usable static web server
+    let server: http.Server;
+
     // Re-usable WebDriver instance
     let driver: WebDriver;
+
+    //  pointer to script execution function
+    let AAScript: Function;
+
+    // Create the web server to get around browser restrictions for local fiels that require GUI interaction.
+    // Note that this is bound to the base directory in order to access the built files.
+    beforeAll(async () => {
+        const file = new Server(".");
+
+        server = http.createServer((req, res) => {
+            req.addListener("end", () => file.serve(req, res)).resume();
+        });
+
+        return new Promise((resolve, reject) => {
+            server.on("listening", resolve);
+            server.on("error", reject);
+
+            server.listen(8080);
+        });
+    });
 
     beforeAll(async () => {
         const capabilities: Capabilities = ((browser: string): Capabilities => {
@@ -19,6 +46,11 @@ describe("aaarray#xbrowser", () => {
                     return Capabilities.ie()
                         .set("ignoreProtectedModeSettings", true)
                         .set("ignoreZoomSetting", true);
+                }
+
+                case "edge": {
+                    require("edgedriver");
+                    return Capabilities.edge();
                 }
 
                 case "safari": {
@@ -43,9 +75,28 @@ describe("aaarray#xbrowser", () => {
         })(BROWSER);
 
         // Create the driver
-        driver = await new Builder().forBrowser(BROWSER).withCapabilities(capabilities).build();
+        driver = await new Builder()
+            .forBrowser(BROWSER)
+            .withCapabilities(capabilities)
+            .build();
+
+        // Assign the script executor
+        AAScript = execScript.bind(driver);
+
         // Navigate the driver to the page for testing
-        await driver.get(`file://${path.resolve(__dirname, "resources", "index.html")}`);
+        await driver.get("http://localhost:8080/test/resources/index.html");
+    });
+
+    afterAll(async () => {
+        return new Promise((resolve, reject) => {
+            server.close((err?: Error) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
     });
 
     afterAll(async () => {
@@ -65,7 +116,11 @@ describe("aaarray#xbrowser", () => {
     });
 
     it("Should wait for AAArray to map", async () => {
-        expect(await driver.executeScript("return AA([1,2,3]).map(function(n) { return n + 1; }).get(0)")).toBe(2);
+        expect(await AAScript("AA([1,2,3]).map(function(n) { return n + 1; }).get(0)")).toBe(2);
     });
-
 });
+
+// Async runner function that is bound to a driver instance to execute a wrapped async script.
+async function execScript(this: WebDriver, script: string): Promise<any> {
+    return this.executeAsyncScript(`var done = arguments[0]; (${script}).then(done)`);
+}

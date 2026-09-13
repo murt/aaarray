eb8987bf86cfd6cb1d704a51f91c0afb322ace89
murt <matt@clever.is>
2026-09-13 14:29:10 +1000

    #835: WIP of browser test integration correctly with vitest and playwright

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,32 +1,35 @@
-import { test, expect } from "@playwright/test";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 
+//import { test, expect } from "@playwright/test";
+import { describe, expect, it, beforeEach } from "vite-plus/test";
+
 const testPage = `file://${resolve(dirname(fileURLToPath(import.meta.url)), "resources/index.html")}`;
 
-test.describe("aaarray#browser", () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto(testPage);
-    });
+describe("aaarray#browser", () => {
+  beforeEach(async ({ page }) => {
+    await page.goto(testPage);
+  });
 
-    test("Should load the test page", async ({ page }) => {
-        expect(await page.title()).toBe("AAArray");
-    });
+  it("Should load the test page", async ({ page }) => {
+    expect(await page.title()).toBe("AAArray");
+  });
 
-    test("Should have an exported AA wrapper function", async ({ page }) => {
-        expect(await page.evaluate(() => typeof (window as any).AA)).toBe("function");
-    });
+  it("Should have an exported AA wrapper function", async ({ page }) => {
+    expect(await page.evaluate(() => typeof (window as any).AA)).toBe("function");
+  });
 
-    test("Should have an exported AAArray class", async ({ page }) => {
-        expect(await page.evaluate(() => typeof (window as any).AAArray)).toBe("function");
-    });
+  it("Should have an exported AAArray class", async ({ page }) => {
+    expect(await page.evaluate(() => typeof (window as any).AAArray)).toBe("function");
+  });
 
-    test("Should wait for AAArray to map", async ({ page }) => {
-        const result = await page.evaluate(() =>
-            (window as any).AA([1, 2, 3])
-                .map((n: number) => n + 1)
-                .get(0)
-        );
-        expect(result).toBe(2);
-    });
+  it("Should wait for AAArray to map", async ({ page }) => {
+    const result = await page.evaluate(() =>
+      (window as any)
+        .AA([1, 2, 3])
+        .map((n: number) => n + 1)
+        .get(0),
+    );
+    expect(result).toBe(2);
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite-plus";
+//import { playwright } from "vite-plus/test/browser-playwright";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
+
 import pkg from "./package.json" with { type: "json" };
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 
 export default defineConfig({
     build: {
@@ -24,6 +27,9 @@ export default defineConfig({
     test: {
         include: ["test/*.test.ts"],
         exclude: ["test/browser.test.ts"],
+        browser: {
+            //provider: playwright()
+        }
     },
     fmt: {
         printWidth: 120,
